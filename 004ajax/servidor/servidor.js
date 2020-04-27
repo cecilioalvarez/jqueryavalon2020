f13d77b695d2b8801955014671760b02ac1cb392
MoisesBravoSantos <moises.bravo19@hotmail.com>
2020-04-27 10:38:55 +0200

    promesas

--- 004ajax/servidor/servidor.js
+++ 004ajax/servidor/servidor.js
@@ -50,8 +50,23 @@ app.get("/fila3", function (req, res) {
 
 
 let libros = [{ "isbn": "1", "titulo": "java" }, { "isbn": "2", "titulo": "phyton" }];
+let libros2 = [{ "isbn": "3", "titulo": "java2" }, { "isbn": "4", "titulo": "phyton2" }];
+let libros3 = [{ "isbn": "5", "titulo": "java5" }, { "isbn": "6", "titulo": "phyton3" }];
+
 app.get("/libros", function (req, res) {
     res.send(libros);
+})
+
+app.get("/libros2", function (req, res) {
+    setTimeout(function () {
+        res.send(libros2);
+    }, 2000)
+})
+
+app.get("/libros3", function (req, res) {
+    setTimeout(function () {
+        res.send(libros3);
+    }, 4000)
 })
 
 app.listen(3000, function () {
