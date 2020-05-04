1687ad80d441c8b0e3f1c163d1d408c6a8bf9e1c
pepegutierrez <pepegutierrezgomez@gmail.com>
2020-05-04 09:38:53 +0200

    fin

--- 004ajax/servidor/servidor.js
+++ 004ajax/servidor/servidor.js
@@ -41,6 +41,27 @@ app.get("/fila3", function (req, res) {
     res.send("<tr><td>luis</td></tr>");
 })
 
+let libros = [{ "isbn": "1", "titulo": "java" }, { "isbn": "2", "titulo": "python" }];
+let libros2 = [{ "isbn": "3", "titulo": "java2" }, { "isbn": "4", "titulo": "python2" }];
+let libros3 = [{ "isbn": "5", "titulo": "java3" }, { "isbn": "6", "titulo": "python3" }];
+
+app.get("/libros", function (req, res) {
+    res.send(libros);
+})
+
+app.get("/libros2", function (req, res) {
+    setTimeout(function () {
+        res.send(libros2);
+    }, 2000);
+})
+
+app.get("/libros3", function (req, res) {
+    setTimeout(function () {
+        res.send(libros3);
+    }, 4000);
+})
+
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
